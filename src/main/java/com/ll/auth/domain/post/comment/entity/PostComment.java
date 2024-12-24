27ee2ba85dabe6821c3debf842f4a2987cc0d479
jhs512 <jangka512@gmail.com>
2024-12-24 11:41:06 +0900

    put

--- src/main/java/com/ll/auth/domain/post/comment/entity/PostComment.java
+++ src/main/java/com/ll/auth/domain/post/comment/entity/PostComment.java
@@ -7,11 +7,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,4 +26,8 @@ public class PostComment extends BaseTime {
 
     @Column(columnDefinition = "TEXT")
     private String content;
+
+    public void modify(String content) {
+        this.content = content;
+    }
 }
